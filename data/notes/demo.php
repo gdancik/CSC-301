b1ae4319bb8c15a40f5ba9799a49e00ddc54e51c
gdancik <garrett.dancik@gmail.com>
2022-01-19 14:23:21 -0500

    Update

--- data/notes/demo.php
+++ data/notes/demo.php
@@ -1,5 +1,4 @@
 <!-- An example php script for demonstrating GET vs POST
-  
   In order to use this script, it must reside on a web server with PHP
   support. The details of writing PHP code are beyond the scope of this
   course, but note that the name-value pairs from an HTML form are passed 
@@ -10,12 +9,13 @@
 
 $txt = "";
 
-if($_GET["txt"])
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') 
 {       
         $txt= $_GET["txt"];
         echo ("<h2>Get request detected</h2>");
 
-} else if($_POST["txt"]) {
+} else {
 
         $txt= $_POST["txt"];
         echo "<h2>Post request detected<br></h2>";
@@ -25,6 +25,4 @@ if($_GET["txt"])
 echo "You entered the following: ". $txt. "<br>";
 
 ?>
-
-
 
